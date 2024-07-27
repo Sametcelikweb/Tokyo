026d28c90b31f3ba6a690542750c991de973e5bf
Sametcelikweb <celikpc.01@hotmail.com>
2024-07-27 08:33:58 +0300

    istenilen revizasyon yapıldı menu.js problemi çözüldü.

--- menu.js
+++ menu.js
@@ -1,30 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuButton = document.getElementById('menu');
     const navbar = document.getElementById('navbar');
-    const icon = document.getElementById('icon')
-    const countent = document.getElementById('countent')
-    var x=0
+    const icon = document.getElementById('icon');
+    const countent = document.getElementById('countent');
+   
+    countent.style.marginTop = '0';
+    countent.style.transition = 'none';
 
-    
+    window.requestAnimationFrame(() => {
+        countent.style.transition = '1s ease';
+    });
+    countent.style.transition = '1s ease';
+    var x=0;
+
+       
         menuButton.addEventListener('click', function() {
             if (x == 0) 
             {
-                navbar.style.maxHeight = '200px';
-                navbar.style.overflow = 'visiable';
-                x=1;
-                icon.className = 'fa-solid fa-x';
-                countent.style.marginTop = '200px';
                 countent.style.transition = '1s ease';
+                navbar.style.height = '200px';
+                countent.style.marginTop = '200px';
+                icon.className = 'fa-solid fa-x';
+                x=1;
             } 
             else 
-            {
-                navbar.style.maxHeight = '0px';
-                x=0;
+            {   
+                navbar.style.height = '0px';
+                countent.style.marginTop = '0px';
                 icon.className = 'fa-solid fa-bars';
-                countent.style.marginTop = '0px';   
+                x=0;
             }
     
         });
-
-
 });
